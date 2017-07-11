7121442e3590f7fbfe7ab898036d37004ebf24f6
TARKZiM <tom8476oo@gmail.com>
2017-07-11 22:46:59 +0800

    t6d: init update for t6dwg, remove unused overlay

--- init/init_t6d.cpp
+++ init/init_t6d.cpp
@@ -51,6 +51,7 @@ void common_properties()
 {
     property_set("rild.libargs", "-d /dev/smd0");
     property_set("rild.libpath", "/system/lib/libril-qc-qmi-1.so");
+    property_set("rild_dsda.libpath", "/system/lib/libril-qc-qmi-qsc.so");
 }
 
 void cdma_properties(char const default_cdma_sub[], char const default_network[])
@@ -58,12 +59,6 @@ void cdma_properties(char const default_cdma_sub[], char const default_network[]
     property_set("ro.telephony.default_network", default_network);
     property_set("persist.radio.snapshot_enabled", "1");
     property_set("persist.radio.snapshot_timer", "22");
-}
-
-void gsm_properties(char const default_network[])
-{
-    property_set("ro.telephony.default_network", default_network);
-    property_set("telephony.lteOnGsmDevice", "1");
 }
 
 void vendor_load_properties()
@@ -78,57 +73,40 @@ void vendor_load_properties()
 
     bootmid = property_get("ro.boot.mid");
 
-    if (bootmid == "0P3P10000") {
-        /* t6vzw (t6wl) */
+    if (bootmid == "0P3P20000") {
+        /* t6dwg (t6dwg) */
         common_properties();
-        cdma_properties("0", "8");
-        property_override("ro.product.model", "HTC6600LVW");
-        property_override("ro.build.fingerprint", "VERIZON/HTCOneMaxVZW/t6wl:4.4.2/KOT49H/333209.2:user/release-keys");
-        property_override("ro.build.description", "3.09.605.2 CL333209 release-keys");
-        property_override("ro.product.device", "t6vzw");
-        property_override("ro.build.product", "t6vzw");
+        cdma_properties("0", "7");
+        property_override("ro.product.model", "HTC 809d");
+        property_override("ro.build.fingerprint", "htc/htccn_chs_ct/t6dwg:5.0.2/LRX22G/524896.8:user/release-keys");
+        property_override("ro.build.description", "3.22.1401.8 CL524896 release-keys");
+        property_override("ro.product.device", "t6dwg");
+        property_override("ro.build.product", "t6dwg");
+        property_override("ro.baseband.arch", "dsda");
         property_set("ro.telephony.default_cdma_sub", "0");
         property_set("ro.ril.enable.sdr", "0");
-        property_set("ro.ril.vzw.feature", "1");
-        property_set("ro.ril.oem.ecclist", "911,*911,#911");
+        property_set("persist.radio.multisim.config", "dsda");
+        property_set("ro.ril.oem.ecclist", "110,112,119,120,911,999");
+        property_set("ro.ril.oem.nosim.ecclist", "110,112,119,120,911,999,08,000,118,122");
+        property_set("ro.ril.oem.normalcall.ecclist", "110,119,120,999");
         property_set("ro.ril.enable.a52", "0");
         property_set("ro.ril.enable.a53", "1");
-        property_set("ro.ril.enable.gea3", "1");
-        property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420");
-        property_set("ro.ril.enable.managed.roaming", "1");
-        property_set("ro.ril.oem.show.act", "0");
-        property_set("ro.ril.set.mtusize", "1428");
-        property_set("ro.ril.wp.feature", "1");
-        property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
-        property_set("ro.gsm.data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
-        property_set("ro.gsm.2nd_data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
-        property_set("ro.config.svlte1x", "true");
-        property_set("ro.telephony.get_imsi_from_sim", "true");
-        property_set("ro.ril.def.agps.mode", "6");
-    } else if (bootmid == "0P3P70000") {
-        /* t6spr (t6whl) */
+        property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420,27202");
+        property_set("ro.ril.set.mtusize", "1420");
+    } else if (bootmid == "0P3P30000") {
+        /* t6dug (t6dug) */
         common_properties();
-        cdma_properties("1", "8");
-        property_override("ro.product.model", "One Max");
-        property_override("ro.build.fingerprint", "htc/sprint_wwe/t6whl:4.4.2/KOT49H/335898.5:user/release-keys");
-        property_override("ro.build.description", "3.02.651.5 CL335898 release-keys");
-        property_override("ro.product.device", "t6spr");
-        property_override("ro.build.product", "t6spr");
+        cdma_properties("0", "7");
+        property_override("ro.product.model", "HTC 8060");
+        property_override("ro.build.fingerprint", "htc/htccn_chs_cmcc/t6dug:5.0.2/LRX22G/524896.8:user/release-keys");
+        property_override("ro.build.description", "3.22.1403.8 CL524896 release-keys");
+        property_override("ro.product.device", "t6dug");
+        property_override("ro.build.product", "t6dug");
         property_override("telephony.sms.pseudo_multipart", "1");
         property_override("persist.radio.mode_pref_nv10", "1");
         property_override("persist.radio.dont_use_dsd", "true");
         property_set("ro.ril.set.mtusize", "1422");
         property_set("ro.ril.svdo", "true");
-    } else {
-        /* t6 */
-        common_properties();
-        gsm_properties("9");
-        property_override("ro.product.model", "One Max");
-        property_override("ro.build.fingerprint", "htc/htc_europe/t6ul:5.0.2/LRX22G/525693.7:user/release-keys");
-        property_override("ro.build.description", "4.13.401.7 CL525693 release-keys");
-        property_override("ro.product.device", "t6");
-        property_override("ro.build.product", "t6");
     }
 
     device = property_get("ro.product.device");
